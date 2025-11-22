84bc96412f73ae9286536065a342f4e04c3c3b5e
Alok3449 <127528419+Alok3449@users.noreply.github.com>
2025-11-22 07:51:24 +0530

    Add missing dotenv package

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: `${process.env.API_BASE_URL}`,
 });
 
 api.interceptors.request.use(config => {
